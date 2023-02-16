2ed99ec007df1f93c8ea89dc17c247ba009fdb3b
dva-ren <iiuzrw@gmail.com>
2023-02-16 12:57:30 +0800

    chore: change api address

--- src/composables/requests.ts
+++ src/composables/requests.ts
@@ -26,7 +26,7 @@ class HttpRequest {
   constructor() {
     this.baseUrl = 'http://localhost:4001/'
     if (import.meta.env.MODE === 'production')
-      this.baseUrl = 'https://api.dvaren.top/admin'
+      this.baseUrl = 'https://admin-api.ddnsto.com/'
   }
 
   getInsideConfig() {
